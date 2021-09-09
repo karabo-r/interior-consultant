dcbbac9a70b5c6c7afe75dcfef1791d4c50f86f5
Karabo Rabothata <88734119+karabo-r@users.noreply.github.com>
2021-09-09 04:31:56 -0700

    Revert "Refactor js file"

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
-const displayMenu = () => {
-  document.querySelector(".fa-bars").style.visibility = "hidden";
-  document.querySelector(".logo").style.visibility = "hidden";
-  document.querySelector("main").style.visibility = "hidden";
-  document.querySelector("nav").style.visibility = "inherit";
-  document.querySelector(".fa-times").style.display = "inherit";
-};
+const menuBtn = document.querySelector(".fa-bars");
+const exitBtn = document.querySelector(".fa-times");
+const logo = document.querySelector(".logo");
+const navBar = document.querySelector("nav");
+const mainSec = document.querySelector("main");
 
-const hideMenu = () => {
-  document.querySelector(".fa-bars").style.visibility = "";
-  document.querySelector(".logo").style.visibility = "";
-  document.querySelector("main").style.visibility = "";
-  document.querySelector("nav").style.visibility = "";
-  document.querySelector(".fa-times").style.display = "";
-};
+function displayMenu() {
+  menuBtn.addEventListener("click", () => {
+    logo.style.display = "none";
+    menuBtn.style.display = "none";
+    exitBtn.style.display = "inherit";
+    navBar.style.visibility = "inherit";
+    mainSec.style.display = "none";
+  });
+  exitBtn.addEventListener("click", () => {
+    logo.style.display = "inherit";
+    menuBtn.style.display = "inherit";
+    exitBtn.style.display = "none";
+    navBar.style.visibility = "hidden";
+    mainSec.style.display = "inherit";
+  });
+}
+
+displayMenu();
